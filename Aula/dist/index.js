90cf29796d97674d524fc9b0b3277a006868e0f4
guilherme.orbem <guilherme.orbem@unibave.net>
2023-03-07 15:58:25 -0300

    Exemplo da aula

--- Aula/dist/index.js
+++ Aula/dist/index.js
@@ -1,3 +1,28 @@
 "use strict";
-console.log("Funciona");
+class Time {
+    constructor(vitorias, derrotas, qtd_jogadores, tecnico, nome) {
+        this._vitorias = vitorias;
+        this._derrotas = derrotas;
+        this.qtd_jogadores = qtd_jogadores;
+        this.tecnico = tecnico;
+        this.nome = nome;
+    }
+    get vitorias() {
+        return this._vitorias;
+    }
+    set vitorias(vitorias) {
+        this._vitorias = vitorias;
+    }
+    get derrotas() {
+        return this._derrotas;
+    }
+}
+let time = new Time(0, 0, 24, "Felipão", "Parmera");
+console.log("Numero de Vitórias " + time.vitorias);
+time.vitorias = 10;
+console.log("Derrotas ", time.derrotas);
+console.log("Quantidade de Jogadores ", time.qtd_jogadores);
+console.log("Técnico ", time.tecnico);
+console.log("Nome ", time.nome);
+console.log("Numero de Vitórias " + time.vitorias);
 //# sourceMappingURL=index.js.map
--- Aula/src/index.ts
+++ Aula/src/index.ts
@@ -1,1 +1,42 @@
-console.log("Funciona")
+class Time {
+    private _vitorias: number
+    protected _derrotas: number
+    qtd_jogadores: number
+    tecnico: string
+    nome: string
+
+    constructor(vitorias: number, derrotas: number,
+        qtd_jogadores: number, tecnico: string,
+        nome: string) {
+
+        this._vitorias = vitorias
+        this._derrotas = derrotas
+        this.qtd_jogadores = qtd_jogadores
+        this.tecnico = tecnico
+        this.nome = nome
+    }
+
+    get vitorias(): number {
+        return this._vitorias;
+    }
+
+    set vitorias(vitorias: number) {
+        this._vitorias = vitorias
+    }
+
+    get derrotas(): number {
+        return this._derrotas
+    }
+
+}
+
+let time = new Time(0, 0, 24, "Felipão", "Parmera")
+
+console.log("Numero de Vitórias " + time.vitorias)
+time.vitorias = 10;
+
+console.log("Derrotas " , time.derrotas)
+console.log("Quantidade de Jogadores ",time.qtd_jogadores)
+console.log("Técnico ",time.tecnico)
+console.log("Nome ",time.nome)
+console.log("Numero de Vitórias " + time.vitorias)
